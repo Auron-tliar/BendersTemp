0f47dfcb092e159c3c556db157e765fbcfa0c17c
Unknown <parcos@outlook.com>
2018-11-15 22:10:14 +0100

    Added ML-Agents
    Added agent classes

---
+++ Assets/Scripts/AIAgent.cs
@@ -0,0 +1,17 @@
+﻿using MLAgents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAgent : Agent {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/BenderAcademy.cs
@@ -0,0 +1,8 @@
+﻿using MLAgents;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BenderAcademy : Academy {
+
+}
