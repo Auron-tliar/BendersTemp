13f4749ebd2ab3955bdb98c0087d351fd3278975
Dmitry Gnatyshak <dmitry.gnatyshak@gmail.com>
2018-11-27 14:48:57 +0100

    AI navagent bug

--- Assets/Scripts/Bender.cs
+++ Assets/Scripts/Bender.cs
@@ -203,7 +203,10 @@ public abstract class Bender : MonoBehaviour
 
     public void StartAbility(States state, int number)
     {
-        NavAgent.isStopped = true;
+        if (_owner.Type == PlayerController.PlayerTypes.Human)
+        {
+            NavAgent.isStopped = true;
+        }
         BenderAnimator.SetBool("Ability", true);
         State = state;
         AbilitiesPS[number].Play();
@@ -225,7 +228,10 @@ public abstract class Bender : MonoBehaviour
         BenderAnimator.SetTrigger("Hit");
         State = States.Recovering;
         BenderAnimator.SetBool("Moving", false);
-        NavAgent.isStopped = true;
+        if (_owner.Type == PlayerController.PlayerTypes.Human)
+        {
+            NavAgent.isStopped = true;
+        }
     }
 
     public void GotRevived()
@@ -236,7 +242,10 @@ public abstract class Bender : MonoBehaviour
     public void Recovered()
     {
         State = States.Idle;
-        NavAgent.isStopped = false;
+        if (_owner.Type == PlayerController.PlayerTypes.Human)
+        {
+            NavAgent.isStopped = false;
+        }
     }
 
     public void GotSelected()
@@ -258,7 +267,10 @@ public abstract class Bender : MonoBehaviour
         _audioSource.PlayOneShot(AbilitySound);
         State = States.Idle;
         BenderAnimator.SetBool("Ability", false);
-        NavAgent.isStopped = false;
+        if (_owner.Type == PlayerController.PlayerTypes.Human)
+        {
+            NavAgent.isStopped = false;
+        }
     }
 
     public void FootstepSound()
