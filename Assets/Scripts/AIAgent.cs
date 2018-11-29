cdc1666f0b26de15dedfae6ea099baf951f20502
Unknown <marek.merten@est.fib.upc.edu>
2018-11-29 11:56:15 +0100

    AI agent: perform random actions

--- Assets/Scripts/AIAgent.cs
+++ Assets/Scripts/AIAgent.cs
@@ -22,30 +22,36 @@ public class AIAgent : Agent {
         int width = terrain.terrainData.heightmapWidth;
         int height = terrain.terrainData.heightmapHeight;
         Vector3 sampleScale = terrain.terrainData.heightmapScale;
-    
-        float[,] heights = terrain.terrainData.GetHeights(0, 0, width, height);
-        float[] flattenedHeights = heights.Cast<float>().ToArray();
 
-        AddVectorObs(flattenedHeights);
+        if (false)
+        {
+            float[,] heights = terrain.terrainData.GetHeights(0, 0, width, height);
+            float[] flattenedHeights = heights.Cast<float>().ToArray();
+
+            AddVectorObs(flattenedHeights);
+        }
 
 
         // Enemy grid
-        Vector3 position = terrain.GetPosition();
-        Vector3 size = terrain.terrainData.size;
-        position = position - size/2;
+        if (false) { 
+            Vector3 position = terrain.GetPosition();
+            Vector3 size = terrain.terrainData.size;
+            position = position - size/2;
 
-        float[] enemyGrid = new float[width * height];
-        foreach(var enemy in enemies)
-        {
-            Vector3 enemyPosition = enemy.transform.position;
-            enemyPosition -= position;
+            float[] enemyGrid = new float[width * height];
+            if (enemies != null)
+            foreach(var enemy in enemies)
+            {
+                Vector3 enemyPosition = enemy.transform.position;
+                enemyPosition -= position;
             
-            Vector2Int enemySamplePosition = new Vector2Int((int)(enemyPosition.x / sampleScale.x), (int)(enemyPosition.z / sampleScale.z));
+                Vector2Int enemySamplePosition = new Vector2Int((int)(enemyPosition.x / sampleScale.x), (int)(enemyPosition.z / sampleScale.z));
 
-            enemyGrid[enemySamplePosition.x * width + enemySamplePosition.y] = 1;
+                enemyGrid[enemySamplePosition.x * width + enemySamplePosition.y] = 1;
+            }
+
+            AddVectorObs(enemyGrid);
         }
-
-        AddVectorObs(enemyGrid);
 
 
 
@@ -57,7 +63,7 @@ public class AIAgent : Agent {
         AddReward(-1);
 
         // Killed all enemies
-        if (enemies.Count == 0) {
+        if (enemies != null && enemies.Count == 0) {
             Done();
             AddReward(100);
         }
@@ -80,28 +86,52 @@ public class AIAgent : Agent {
             return;
         }
 
-        if (vectorAction[0] == 1)
-        {
-            bender.StartAbility(Bender.States.Casting1, 0);
-        }
-        else if (vectorAction[1] == 1)
-        {
-            bender.StartAbility(Bender.States.Casting2, 1);
-        }
-        else if (vectorAction[2] == 1)
-        {
-            bender.StartAbility(Bender.States.Casting3, 2);
-        }
+        int selectedAction = MaxIndex(vectorAction);
 
+        switch (selectedAction) {
+            case 1:
+                bender.StartAbility(Bender.States.Casting1, 0); break;
+            case 2:
+                bender.StartAbility(Bender.States.Casting2, 1); break;
+            case 3:
+                bender.StartAbility(Bender.States.Casting3, 2); break;
+            case 4:
+                bender.SpeedInput = 5f; break;
+            case 5:
+                bender.SpeedInput = -5f; break;
+            case 6:
+                bender.RotationSpeedInput = 5f; break;
+            case 7:
+                bender.RotationSpeedInput = -5f; break;
+            default:
+                break;
+        }
+    }
+
+
+
+    private int MaxIndex(float[]  array) {
+        float max = array[0];
+        int maxIndex = 0;
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
     }
 
     public override void AgentOnDone()
     {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
     }
 
     public override void AgentReset()
     {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
     }
 }
